9711a2d4c01f2eb41b91e14743ab7a5b9001da53
44tim44 <timeisrelative94@gmail.com>
2017-10-05 14:26:44 +0200

    Initial commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
+import java.io.File;
+import java.util.*;
+
 /**
  * Created by timmy on 2017-10-05.
  */
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Scanner inFile1 = new Scanner(new File("KeyWestTemp.txt")).useDelimiter("\\s");
+
+        //List<Integer[][]> imageList = new ArrayList<>();
+        HashMap<String, Integer[][]> imageHashMap = new HashMap();
+
+        while (inFile1.hasNext()) {
+            String imageName = inFile1.next();
+            Integer[][] image = new Integer[20][20];
+            for (int y = 0; y < 20; y++) {
+                for (int x = 0; x < 20; x++) {
+                    int value = inFile1.nextInt();
+                    image[x][y] = value;
+                }
+            }
+            //imageList.add(image);
+            imageHashMap.put(imageName,image);
+        }
     }
 }
