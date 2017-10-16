2a162c37323db869697ef124bb1f13551ec4d111
alexnorrman <alexnorrman@gmail.com>
2017-10-16 11:42:17 +0200

    Added javadoc for image.java

--- src/Image.java
+++ src/Image.java
@@ -1,5 +1,6 @@
 /**
- * Created by timmy on 2017-10-12.
+ * @author Timmy Eklund, id15ted@cs.umu.se
+ * @author Alex Norrman, id14ann@cs.umu.se
  */
 public class Image
 {
@@ -13,20 +14,32 @@ public class Image
         this.image = image;
     }
 
+    /**
+     * @return Returns the name of the image
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * @return Returns the image-matrix
+     */
     public Integer[][] getImage()
     {
         return image;
     }
 
+    /**
+     * @return Returns an int that represents the correct expression of the image
+     */
     public int getExpression() {
         return expression;
     }
 
+    /**
+     * @param expression Sets an in that represents the correct expression of the image
+     */
     public void setExpression(int expression) {
         this.expression = expression;
     }
